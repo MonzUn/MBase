2ce726d95d1b05a5097745ba04e1183056f92fe8
MonzUn <daniel-bengtsson@live.com>
2018-03-04 10:39:54 +0100

    Resolved TODO; initialization, update and shutdown of internal global MEngine systems have been separated to it's own file in order to avoid cluttering of MEngine.cpp

--- source/mengine.cpp
+++ source/mengine.cpp
@@ -1,14 +1,8 @@
 #include "interface/mengine.h"
-#include "interface/mengineUtility.h"
-#include "mengineComponentManagerInternal.h"
-#include "mengineConfigInternal.h"
-#include "mengineEntityManagerInternal.h"
 #include "mengineGraphicsInternal.h"
-#include "mengineInternalComponentsInternal.h"
 #include "mengineInputInternal.h"
+#include "mengineGlobalSystems.h"
 #include "mengineSystemManagerInternal.h"
-#include "mengineTextInternal.h"
-#include "mengineUtilityInternal.h"
 #include <MUtilityLog.h>
 #include <MUtilitySystem.h>
 #include <SDL.h>
@@ -43,15 +37,7 @@ bool MEngine::Initialize(const char* appName, int32_t windowWidth, int32_t windo
 		return false;
 	}
 
-	// TODODB: Make a separate file for all system initialization/shutdown so that we can avoid cluttering this file with them and their includes
-	MEngineUtility::Initialize();
-	MEngineEntityManager::Initialize();
-	MEngineComponentManager::Initialize();
-	MEngineInternalComponents::Initialize();
-	MEngineInput::Initialize();
-	MEngineText::Initialize();
-	MEngineConfig::Initialize();
-	MEngineSystemManager::Initialize();
+	MEngineGlobalSystems::Start();
 
 	MLOG_INFO("MEngine initialized successfully", LOG_CATEGORY_GENERAL);
 
@@ -63,15 +49,7 @@ void MEngine::Shutdown()
 {
 	assert(IsInitialized() && "Calling SDLWrapper::Shutdown but it has not yet been initialized");
 
-	MEngineSystemManager::Shutdown();
-	MEngineConfig::Shutdown();
-	MEngineText::Shutdown();
-	MEngineInput::Shutdown();
-	MEngineInternalComponents::Shutdown();
-	MEngineComponentManager::Shutdown();
-	MEngineEntityManager::Shutdown();
-	MEngineGraphics::Shutdown();
-	MEngineUtility::Shutdown();
+	MEngineGlobalSystems::Stop();
 
 	m_Initialized = false;
 	SDL_Quit();
@@ -92,8 +70,7 @@ bool MEngine::ShouldQuit()
 
 void MEngine::Update()
 {
-	MEngineUtility::Update();
-	MEngineInput::Update();
+	MEngineGlobalSystems::Update();
 
 	SDL_Event event;
 	while (SDL_PollEvent(&event) != 0)
---
+++ source/mengineGlobalSystems.h
@@ -0,0 +1,45 @@
+#pragma once
+#include "interface/mengineUtility.h"
+#include "mengineComponentManagerInternal.h"
+#include "mengineConfigInternal.h"
+#include "mengineEntityManagerInternal.h"
+#include "mengineGraphicsInternal.h"
+#include "mengineInternalComponentsInternal.h"
+#include "mengineInputInternal.h"
+#include "mengineSystemManagerInternal.h"
+#include "mengineTextInternal.h"
+#include "mengineUtilityInternal.h"
+
+namespace MEngineGlobalSystems
+{
+	void Start()
+	{
+		MEngineUtility::Initialize();
+		MEngineEntityManager::Initialize();
+		MEngineComponentManager::Initialize();
+		MEngineInternalComponents::Initialize();
+		MEngineInput::Initialize();
+		MEngineText::Initialize();
+		MEngineConfig::Initialize();
+		MEngineSystemManager::Initialize();
+	}
+
+	void Stop()
+	{
+		MEngineSystemManager::Shutdown();
+		MEngineConfig::Shutdown();
+		MEngineText::Shutdown();
+		MEngineInput::Shutdown();
+		MEngineInternalComponents::Shutdown();
+		MEngineComponentManager::Shutdown();
+		MEngineEntityManager::Shutdown();
+		MEngineGraphics::Shutdown();
+		MEngineUtility::Shutdown();
+	}
+
+	void Update()
+	{
+		MEngineUtility::Update();
+		MEngineInput::Update();
+	}
+}
