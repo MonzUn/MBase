7b57619face6b7c9b46d98aeb75cb8ef16de22cf
MonzUn <daniel-bengtsson@live.com>
2018-03-09 17:35:05 +0100

    Console fontID are not set by host application upon initialization

--- source/mengineConsole.cpp
+++ source/mengineConsole.cpp
@@ -19,13 +19,19 @@ typedef std::unordered_map<std::string, MEngineConsoleCallback> CommandMap;
 constexpr char DELIMITER = ' ';
 constexpr int32_t INPUT_TEXTBOX_HEIGHT = 25;
 
+void CreateComponents();
+void DestroyComponents();
+
 namespace MEngineConsole
 {
 	CommandMap* m_Commands = nullptr;
 	MEngine::EntityID m_BackgroundID	= INVALID_MENGINE_ENTITY_ID;
 	MEngine::EntityID m_OutputTextboxID	= INVALID_MENGINE_ENTITY_ID;
 	MEngine::EntityID m_InputTextboxID	= INVALID_MENGINE_ENTITY_ID;
+	MEngine::MEngineFontID m_InputFont	= INVALID_MENGINE_FONT_ID;
+	MEngine::MEngineFontID m_OutputFont = INVALID_MENGINE_FONT_ID;
 	bool isActive = true;
+	bool m_InitializedByHost = false;
 	int32_t m_OutputTextBoxOriginalHeight = -1;
 }
 
@@ -33,7 +39,23 @@ using namespace MEngine;
 using namespace MEngineConsole;
 using namespace PredefinedColors;
 
-// ---------- INTERNAL ----------
+// ---------- INTERFACE ----------
+
+bool MEngine::InitializeConsole(MEngineFontID inputFontID, MEngineFontID outputFontID)
+{
+	bool result = false;
+	if (!m_InitializedByHost)
+	{
+		m_InitializedByHost = true;
+		m_InputFont			= inputFontID;
+		m_OutputFont		= outputFontID;
+		CreateComponents();
+	}
+	else
+		MLOG_WARNING("Attempted to initialize console multiple times", LOG_CATEGROY_CONSOLE);
+
+	return result;
+}
 
 bool MEngine::RegisterCommand(const std::string& commandName, MEngineConsoleCallback callback)
 {
@@ -114,10 +136,54 @@ bool MEngine::ExecuteCommand(const std::string& command, std::string* outRespons
 	return result;
 }
 
-void MEngine::SetConsoleActive(bool active)
+bool MEngine::SetFont(MEngineFontID ID, ConsoleFont fontToSet)
 {
+	if (!m_InitializedByHost)
+	{
+		MLOG_WARNING("Attempted to set font before initializing the console", LOG_CATEGROY_CONSOLE);
+		return false;
+	}
+
+	bool result = !IsFontIDValid(ID);
+	if (result)
+	{
+		switch (fontToSet)
+		{
+		case MEngine::ConsoleFont::Input:
+		{
+			m_InputFont = ID;
+		} break;
+		case MEngine::ConsoleFont::Output:
+		{
+			m_OutputFont = ID;
+		} break;
+		case MEngine::ConsoleFont::Both:
+		{
+			m_InputFont = ID;
+			m_OutputFont = ID;
+		} break;
+		default:
+			break;
+		}
+
+		CreateComponents();
+	}
+	else
+		MLOG_WARNING("Attempted to set console font using an invalid font ID; ID = " << ID, LOG_CATEGROY_CONSOLE);
+
+	return result;
+}
+
+bool MEngine::SetConsoleActive(bool active)
+{
+	if (!m_InitializedByHost)
+	{
+		MLOG_WARNING("Attempted to acitvate/deactivate console without it being initialized", LOG_CATEGROY_CONSOLE);
+		return false;
+	}
+
 	if (isActive == active)
-		return;
+		return false;
 
 	RectangleRenderingComponent* mainBackground = static_cast<RectangleRenderingComponent*>(GetComponentForEntity(RectangleRenderingComponent::GetComponentMask(), m_BackgroundID));
 	TextComponent* outputText = static_cast<TextComponent*>(GetComponentForEntity(TextComponent::GetComponentMask(), m_OutputTextboxID));
@@ -143,6 +209,7 @@ void MEngine::SetConsoleActive(bool active)
 		inputText->StopEditing();
 	}
 	isActive = active;
+	return true;
 }
 
 // ---------- INTERNAL ----------
@@ -150,7 +217,59 @@ void MEngine::SetConsoleActive(bool active)
 void MEngineConsole::Initialize()
 {
 	m_Commands = new CommandMap();
+}
 
+void MEngineConsole::shutdown()
+{
+	delete m_Commands;
+	DestroyComponents();
+}
+
+void MEngineConsole::Update()
+{
+	if (m_InitializedByHost)
+	{
+		if (isActive)
+		{
+			const TextComponent* inputText = static_cast<const TextComponent*>(GetComponentForEntity(TextComponent::GetComponentMask(), m_InputTextboxID));
+			const TextComponent* outputText = static_cast<const TextComponent*>(GetComponentForEntity(TextComponent::GetComponentMask(), m_OutputTextboxID));
+			if (*inputText->Text != "" && IsInputString(inputText->Text) && KeyReleased(MKEY_MAIN_ENTER) || KeyReleased(MKEY_NUMPAD_ENTER))
+			{
+				std::string response;
+				ExecuteCommand(*inputText->Text, &response);
+				*outputText->Text += ">" + *inputText->Text + '\n' + " - " + response + '\n';
+
+				*inputText->Text = "";
+			}
+
+			// Write new log messages
+			std::string newMessages;
+			if (MUtilityLog::FetchUnreadMessages(newMessages))
+				*outputText->Text += newMessages;
+
+			// Keep the output text within the console
+			PosSizeComponent* outputTextPos = static_cast<PosSizeComponent*>(GetComponentForEntity(PosSizeComponent::GetComponentMask(), m_OutputTextboxID));
+			uint16_t fullTextHeight = GetTextHeight(outputText->FontID, outputText->Text->c_str());
+			int32_t diff = fullTextHeight + outputTextPos->PosY - m_OutputTextBoxOriginalHeight;
+			if (diff > 0)
+			{
+				outputTextPos->PosY -= diff;
+				outputTextPos->Height += diff;
+			}
+		}
+
+		// TODODB: This comma gets placed in the input string if the textbox is taking input; fix that
+		if (KeyReleased(MKEY_NUMPAD_COMMA)) // TODODB: Check against some action here when such a system has been built 
+			SetConsoleActive(!isActive);
+	}
+}
+
+// ---------- LOCAL ----------
+
+void CreateComponents()
+{
+	DestroyComponents();
+	
 	m_OutputTextBoxOriginalHeight = GetWindowHeight() / 3;
 	int32_t fullWidth = GetWindowWidth();
 
@@ -158,61 +277,27 @@ void MEngineConsole::Initialize()
 	AddComponentsToEntity(PosSizeComponent::GetComponentMask() | RectangleRenderingComponent::GetComponentMask(), m_BackgroundID);
 
 	PosSizeComponent* posSize = static_cast<PosSizeComponent*>(GetComponentForEntity(PosSizeComponent::GetComponentMask(), m_BackgroundID));
-	posSize->PosX	= 0;
-	posSize->PosY	= 0;
-	posSize->PosZ	= 1U;
-	posSize->Width	= fullWidth;
+	posSize->PosX = 0;
+	posSize->PosY = 0;
+	posSize->PosZ = 1U;
+	posSize->Width = fullWidth;
 	posSize->Height = m_OutputTextBoxOriginalHeight;
 
 	RectangleRenderingComponent* background = static_cast<RectangleRenderingComponent*>(GetComponentForEntity(RectangleRenderingComponent::GetComponentMask(), m_BackgroundID));
-	background->FillColor	= ColorData(0, 128, 0, 128);
+	background->FillColor = ColorData(0, 128, 0, 128);
 
-	m_OutputTextboxID	= CreateTextBox(0, 0, fullWidth, m_OutputTextBoxOriginalHeight - INPUT_TEXTBOX_HEIGHT, 0U);
-	m_InputTextboxID	= CreateTextBox(0, m_OutputTextBoxOriginalHeight - INPUT_TEXTBOX_HEIGHT, fullWidth, INPUT_TEXTBOX_HEIGHT, 0U, true, "", Colors[TRANSPARENT], Colors[BLUE]);
+	m_OutputTextboxID = CreateTextBox(0, 0, fullWidth, m_OutputTextBoxOriginalHeight - INPUT_TEXTBOX_HEIGHT, m_OutputFont, 0U);
+	m_InputTextboxID = CreateTextBox(0, m_OutputTextBoxOriginalHeight - INPUT_TEXTBOX_HEIGHT, fullWidth, INPUT_TEXTBOX_HEIGHT, m_InputFont, 0U, true, "", Colors[TRANSPARENT], Colors[BLUE]);
 
 	SetConsoleActive(false);
 }
 
-void MEngineConsole::shutdown()
+void DestroyComponents()
 {
-	delete m_Commands;
-	DestroyEntity(m_BackgroundID);
-	DestroyEntity(m_OutputTextboxID);
-	DestroyEntity(m_InputTextboxID);
-}
-
-void MEngineConsole::Update()
-{
-	if (isActive)
-	{
-		const TextComponent* inputText = static_cast<const TextComponent*>(GetComponentForEntity(TextComponent::GetComponentMask(), m_InputTextboxID));
-		const TextComponent* outputText = static_cast<const TextComponent*>(GetComponentForEntity(TextComponent::GetComponentMask(), m_OutputTextboxID));
-		if (*inputText->Text != "" && IsInputString(inputText->Text) && KeyReleased(MKEY_MAIN_ENTER) || KeyReleased(MKEY_NUMPAD_ENTER))
-		{
-			std::string response;
-			ExecuteCommand(*inputText->Text, &response);
-			*outputText->Text += ">" + *inputText->Text + '\n' + " - " + response + '\n';
-
-			*inputText->Text = "";
-		}
-
-		// Write new log messages
-		std::string newMessages;
-		if (MUtilityLog::FetchUnreadMessages(newMessages))
-			*outputText->Text += newMessages;
-
-		// Keep the output text within the console
-		PosSizeComponent* outputTextPos = static_cast<PosSizeComponent*>(GetComponentForEntity(PosSizeComponent::GetComponentMask(), m_OutputTextboxID));
-		uint16_t fullTextHeight = GetTextHeight(outputText->FontID, outputText->Text->c_str());
-		int32_t diff = fullTextHeight + outputTextPos->PosY - m_OutputTextBoxOriginalHeight;
-		if (diff > 0)
-		{
-			outputTextPos->PosY		-= diff;
-			outputTextPos->Height	+= diff;
-		}
-	}
-
-	// TODODB: This comma gets placed in the input string if the textbox is taking input; fix that
-	if (KeyReleased(MKEY_NUMPAD_COMMA)) // TODODB: Check against some action here when such a system has been built 
-		SetConsoleActive(!isActive);
+	if(m_BackgroundID != INVALID_MENGINE_ENTITY_ID)
+		DestroyEntity(m_BackgroundID);
+	if (m_OutputTextboxID != INVALID_MENGINE_ENTITY_ID)
+		DestroyEntity(m_OutputTextboxID);
+	if (m_InputTextboxID != INVALID_MENGINE_ENTITY_ID)
+		DestroyEntity(m_InputTextboxID);
 }
