b621d0c5e1a765bb7b1a65ac2ebabd77aebb0f5b
MonzUn <daniel-bengtsson@live.com>
2018-03-22 19:13:57 +0100

    Added Hide/Show functions for buttons and text boxes

--- source/MEngineEntityFactory.cpp
+++ source/MEngineEntityFactory.cpp
@@ -1,8 +1,11 @@
 #include "Interface/MEngineEntityFactory.h"
 #include "Interface/MEngineEntityManager.h"
+#include <MUtilityPlatformDefinitions.h>
 
 using namespace MEngine;
 using namespace PredefinedColors;
+
+#define LOG_CATEGORY_ENTITY_FACTORY "MEngineEntityFactory"
 
 EntityID MEngine::CreateButton(int32_t posX, int32_t posY, int32_t width, int32_t height, std::function<void()> callback, uint32_t posZ, TextureID texture, MEngineFontID fontID, const std::string& text, TextAlignment textAlignment)
 {
@@ -63,4 +66,72 @@ EntityID MEngine::CreateTextBox(int32_t posX, int32_t posY, int32_t width, int32
 	}
 
 	return ID;
+}
+
+bool MEngine::ShowButton(EntityID ID)
+{
+#if COMPILE_MODE == COMPILE_MODE_DEBUG
+	if ((GetComponentMask(ID) & BUTTON_ENTITY_MASK) != BUTTON_ENTITY_MASK)
+	{
+		MLOG_WARNING("Attempted to show button using an EntityID not belonging to a button entity", LOG_CATEGORY_ENTITY_FACTORY);
+		return false;
+	}
+#endif
+
+	static_cast<ButtonComponent*>(MEngine::GetComponentForEntity(ButtonComponent::GetComponentMask(), ID))->IsActive = true;
+	static_cast<TextComponent*>(MEngine::GetComponentForEntity(TextComponent::GetComponentMask(), ID))->RenderIgnore = false;
+	static_cast<TextureRenderingComponent*>(MEngine::GetComponentForEntity(TextureRenderingComponent::GetComponentMask(), ID))->RenderIgnore = false;
+
+	return true;
+}
+
+bool MEngine::HideButton(EntityID ID)
+{
+#if COMPILE_MODE == COMPILE_MODE_DEBUG
+	if ((GetComponentMask(ID) & BUTTON_ENTITY_MASK) != BUTTON_ENTITY_MASK)
+	{
+		MLOG_WARNING("Attempted to hide button using an EntityID not belonging to a button entity", LOG_CATEGORY_ENTITY_FACTORY);
+		return false;
+	}
+#endif
+
+	static_cast<ButtonComponent*>(MEngine::GetComponentForEntity(ButtonComponent::GetComponentMask(), ID))->IsActive = false;
+	static_cast<TextComponent*>(MEngine::GetComponentForEntity(TextComponent::GetComponentMask(), ID))->RenderIgnore = true;
+	static_cast<TextureRenderingComponent*>(MEngine::GetComponentForEntity(TextureRenderingComponent::GetComponentMask(), ID))->RenderIgnore = true;
+
+	return true;
+}
+
+bool MEngine::ShowTextBox(EntityID ID)
+{
+#if COMPILE_MODE == COMPILE_MODE_DEBUG
+	if ((GetComponentMask(ID) & TEXT_BOX_ENTITY_MASK) != TEXT_BOX_ENTITY_MASK)
+	{
+		MLOG_WARNING("Attempted to hide text box using an EntityID not belonging to a text box entity", LOG_CATEGORY_ENTITY_FACTORY);
+		return false;
+	}
+#endif
+
+	static_cast<TextComponent*>(MEngine::GetComponentForEntity(TextComponent::GetComponentMask(), ID))->RenderIgnore = false;
+	static_cast<ButtonComponent*>(MEngine::GetComponentForEntity(ButtonComponent::GetComponentMask(), ID))->IsActive = true;
+	static_cast<RectangleRenderingComponent*>(MEngine::GetComponentForEntity(RectangleRenderingComponent::GetComponentMask(), ID))->RenderIgnore = false;
+
+	return true;
+}
+
+bool MEngine::HideTextBox(EntityID ID)
+{
+#if COMPILE_MODE == COMPILE_MODE_DEBUG
+	if ((GetComponentMask(ID) & TEXT_BOX_ENTITY_MASK) != TEXT_BOX_ENTITY_MASK)
+	{
+		MLOG_WARNING("Attempted to show text box using an EntityID not belonging to a text box entity", LOG_CATEGORY_ENTITY_FACTORY);
+		return false;
+	}
+#endif
+
+	static_cast<TextComponent*>(MEngine::GetComponentForEntity(TextComponent::GetComponentMask(), ID))->RenderIgnore = true;
+	static_cast<ButtonComponent*>(MEngine::GetComponentForEntity(ButtonComponent::GetComponentMask(), ID))->IsActive = false;
+	static_cast<RectangleRenderingComponent*>(MEngine::GetComponentForEntity(RectangleRenderingComponent::GetComponentMask(), ID))->RenderIgnore = true;
+
+	return true;
 }
